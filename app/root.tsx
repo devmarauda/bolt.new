1a5f84cab118474843724a117d690028eaf561fc
damarauda <dan@marauda.io>
2024-12-01 08:14:35 +1100

    aef

--- app/root.tsx
+++ app/root.tsx
@@ -34,7 +34,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+    href: 'https://fonts.googleapis.com/css2?family=GeistMono:wght@400;500;600;700&display=swap',
   },
 ];
 
